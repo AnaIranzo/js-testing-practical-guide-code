ffa6f85500555a61a7251b6845bd6be8a1f1182f
AnaIranzo <aeiranzom@gmail.com>
2023-09-18 18:19:38 +0200

    validation 2 ex finish

--- code/01-starting-setup/basic-testing-frontend/src/util/validation.test.js
+++ code/01-starting-setup/basic-testing-frontend/src/util/validation.test.js
@@ -53,3 +53,26 @@ it('should yield undefined if an string is provided', () =>{
 });
 
 
+it('should throw an error if NaN is provided', () => {
+    const input = NaN;
+    const validationFn = () => validateNumber(input);
+    expect(validationFn).toThrow();
+  });
+  
+  it('should throw an error with a message that contains a reason (invalid number)', () => {
+    const input = NaN;
+    const validationFn = () => validateNumber(input);
+    expect(validationFn).toThrow(/Invalid number/);
+  });
+  
+  it('should throw an error if a non-numeric value is provided', () => {
+    const input = '1';
+    const validationFn = () => validateNumber(input);
+    expect(validationFn).toThrow();
+  });
+  
+  it('should not throw an error, if a number is provided', () => {
+    const input = 1;
+    const validationFn = () => validateNumber(input);
+    expect(validationFn).not.toThrow();
+  });
